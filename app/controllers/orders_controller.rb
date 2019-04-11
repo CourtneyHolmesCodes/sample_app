1eb3f88b27a304a40737a6c32347448e242edc99
courtneyholmescodes <courtneylpayne@gmail.com>
2019-04-11 09:45:14 -0600

    4.3 added controller for orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+ end
+
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def Show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end 
